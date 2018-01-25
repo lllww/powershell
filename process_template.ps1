b50fe3f7fa6a338dd47c1ec36263b17e1b507437
lllww <lllamwaiwing@gmail.com>
2018-01-25 23:58:14 +0800

    Initial commit

---
+++ process_template.ps1
@@ -0,0 +1,18 @@
+﻿# grab relevant lines from file
+$KeyValPairs = Get-Content .\tokens.ini | Where {$_ -like "*=*" }
+
+# join strings together as one big string
+$KeyValPairString = $KeyValPairs -join [Environment]::NewLine
+
+# create hashtable/dictionary from string with ConvertFrom-StringData
+$Dictionary = $KeyValPairString |ConvertFrom-StringData
+
+
+Get-Content .\template.txt |ForEach-Object { 
+    [Regex]::Replace($_, '%(\p{L}+)%', {
+        param($Match)
+
+        # look term up in dictionary
+        return $Dictionary[$Match.Groups[1].Value]
+    }) 
+}
