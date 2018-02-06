3c13ccc9531e7ea1ca9db431eca68fcfabbfb2f3
lllww <lllamwaiwing@gmail.com>
2018-02-06 23:32:56 +0800

    initial commit

---
+++ Get-PublicKey-2.ps1
@@ -0,0 +1,5 @@
+﻿$webRequest = [Net.WebRequest]::Create("https://www.outlook.com")
+try { $webRequest.GetResponse() } catch {}
+$cert = $webRequest.ServicePoint.Certificate
+$bytes = $cert.Export([Security.Cryptography.X509Certificates.X509ContentType]::Cert)
+set-content -value $bytes -encoding byte -path "$pwd\Outlook.Com.cer"
